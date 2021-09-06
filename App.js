5d18114f9c47c83f054a4d88f0ce8a1a9bd9f901
Nicolas Corbalan <outreef@hotmail.com>
2021-09-07 01:01:39 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  label,
+  TouchableOpacity
+} from 'react-native';
 
-export default function App() {
+const SettingsOption = ({label}) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <TouchableOpacity>
+      <Text style={styles.text}>{label}</Text>
+    </TouchableOpacity>
+  )
 }
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={[styles.text, styles.header]}>
+          Settings
+        </Text>
+      </View>
+      <SettingsOption label={'Upadte Profile'} />
+      <SettingsOption label={'Upadte Address'} />
+      <SettingsOption label={'Upadte Notifications Preferences'} />
+      <SettingsOption label={'Sign Out'} />
+    </SafeAreaView>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#102D66',
   },
-});
+  content: {
+  paddingHorizontal: 20,
+  },
+  text: {
+    color: 'white',
+    fontSize: 15,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  }
+})
+
+export default App;
